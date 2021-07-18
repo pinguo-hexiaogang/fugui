d2170fddda22962fd6604147a8e4f8e29aa946d6
hexiaogang <he.yssl@foxmail.com>
2021-07-18 17:08:54 +0800

    add eureka

---
+++ eureka/src/main/java/com/xiaogang/fugui/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.xiaogang.fugui.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author hexiaogang
+ * @date 2021/7/18 3:54 下午
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
